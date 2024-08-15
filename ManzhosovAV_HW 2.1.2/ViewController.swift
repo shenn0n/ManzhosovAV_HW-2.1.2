2823081d216197241fd498822b3c16135206a5cb
shenn0n <shenn0n4412@gmail.com>
2024-08-15 19:29:54 +0300

    1. Work with interface

--- ManzhosovAV_HW 2.1.2/ViewController.swift
+++ ManzhosovAV_HW 2.1.2/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        redView.layer.cornerRadius = 75
+        greenView.layer.cornerRadius = 75
+        yellowView.layer.cornerRadius = 75
+        startButton.layer.cornerRadius = 7
     }
 
-
+    @IBAction func startButtonPressed(_ sender: Any) {
+    }
+    
 }
 
