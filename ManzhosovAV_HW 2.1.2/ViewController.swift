643ec0c0867ea5b587912b780f781770bd7e8e97
shenn0n <shenn0n4412@gmail.com>
2024-08-16 11:36:59 +0300

    ViewController

--- ManzhosovAV_HW 2.1.2/ViewController.swift
+++ ManzhosovAV_HW 2.1.2/ViewController.swift
@@ -14,15 +14,59 @@ class ViewController: UIViewController {
     @IBOutlet var yellowView: UIView!
     @IBOutlet var startButton: UIButton!
     
+    private var state = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         redView.layer.cornerRadius = 75
+        redView.alpha = 0.3
         greenView.layer.cornerRadius = 75
+        greenView.alpha = 0.3
         yellowView.layer.cornerRadius = 75
+        yellowView.alpha = 0.3
         startButton.layer.cornerRadius = 7
     }
 
     @IBAction func startButtonPressed(_ sender: Any) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+//        if redView.alpha < 1 && greenView.alpha < 1 && yellowView.alpha < 1 {
+//            redView.alpha = 1
+//            return
+//        }
+//        if redView.alpha == 1 {
+//            yellowView.alpha = 1
+//            redView.alpha = 0.3
+//            return
+//        }
+//        if yellowView.alpha == 1 {
+//            greenView.alpha = 1
+//            yellowView.alpha = 0.3
+//            return
+//        }
+//        if greenView.alpha == 1 {
+//            redView.alpha = 1
+//            greenView.alpha = 0.3
+//            return
+//        }
+        
+        switch state {
+        case "red":
+            state = "yellow"
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+        case "yellow":
+            state = "green"
+            greenView.alpha = 1
+            yellowView.alpha = 0.3
+        case "green":
+            state = "red"
+            redView.alpha = 1
+            greenView.alpha = 0.3
+        default:
+            state = "red"
+            redView.alpha = 1
+        }
     }
     
 }
